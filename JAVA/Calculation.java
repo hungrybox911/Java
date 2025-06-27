9e458076a9061f9c41f4937f8ebf9d9fb3299e60
Aradhya <aradh01nepal@gmail.com>
2025-06-27 21:42:33 +0545

    Add files via upload

---
+++ JAVA/Calculation.java
@@ -0,0 +1,30 @@
+
+
+public class Calculation {
+public static void main (String[] args) {
+int a=10;
+int b=5;
+int sum = a+b;
+int difference = a-b;
+int multiplication = a*b;
+int divide = a/b;
+int modulus = a%b;
+
+System.out.println("Sum is " +sum);
+System.out.println("Difference is" +difference);
+System.out.println("Multiplication is" +multiplication);
+System.out.println("Divide is " +divide);
+System.out.println("Modulus is " +modulus);
+
+
+
+
+    
+}
+
+
+    
+
+
+
+}
